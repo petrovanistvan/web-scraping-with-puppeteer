61714e022188191efdb0d8c7cdbfb6274ef40885
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-14 20:54:45 +0200

    getting back specific data from the website

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: false}); // launch browser
     const page = await browser.newPage(); // open new page
     await page.goto("https://www.visit1066country.com/destinations/hastings/whats-on") // go to page
+
+    const data = await page.evaluate(function() {
+        const events = document.querySelectorAll("prodListItemWrapper");
+        const array = [];
+
+        for (let i = 0; i < events.length; i++) {
+            array.push({
+                title: events[i].querySelector("ProductDetail").innerText,
+                description: events[i].querySelector("desc").innerText,
+                dates: events[i].querySelector("dates").innerText
+            })
+        }
+
+        return;
+    })
+
+    console.log(data);
 })(); // self executing function
