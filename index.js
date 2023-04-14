1645216d93cf68936fef9b2b5c3b363a27add08a
Istvan Petrovan <petrovanistvan@gmail.com>
2023-04-14 21:20:48 +0200

    changed url and classes

--- index.js
+++ index.js
@@ -5,21 +5,24 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch( { headless: false } )
   const page = await browser.newPage()
-  await page.goto("https://www.visit1066country.com/destinations/hastings/whats-on")
+  await page.goto("https://www.oragyor.hu/kereses?k=ga+2100")
 
 
   const data = await page.evaluate(() => {
      // Create array to store objects:
     const list = [];
     // Get each product container:
-    const items = document.querySelectorAll(".productList > li");
+    const items = document.querySelectorAll(".product-name-container");
+    console.log(items);
 
     // Each iteration of loop then pushes an object to the array:
     for (i=0; i<items.length; i++) {
       list.push({
-        event: items[i].querySelector(".ProductName a").textContent,
-        dates: items[i].querySelector(".dates").innerHTML,
-        link: 'https://www.visit1066country.com'+items[i].querySelector(".ProductDetail").getAttribute('href'),
+        // event: items[i].querySelector(".ProductName a").textContent,
+        // dates: items[i].querySelector(".dates").innerHTML,
+        // link: 'https://www.visit1066country.com'+items[i].querySelector(".ProductDetail").getAttribute('href'),
+        brandName: items[i].querySelector(".brandname").textContent,
+        productCode: items[i].querySelector(".productcode").textContent
       })
     }
     // Return the array of objects (console.log here will print in the browser!)
